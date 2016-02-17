48b56142f6a8e0c9f2cabd9c2ec8d6e28852c57e
haibo <buaayhb@163.com>
2016-02-17 10:42:39 +0800

    add api surpport

--- www/urls.py
+++ www/urls.py
@@ -3,15 +3,25 @@
 
 __author__ = 'Haibo Yu'
 
-import logging
+import os, re, time, base64, hashlib, logging
 
-from transwarp.web import get, view
+from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
+
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 
 from models import User, Blog, Comment
 
 @view('blogs.html')
 @get('/')
 def index():
-	blogs = Blog.find_all()
-	user = User.find_first('where email = ?', 'admin@example.com')
-	return dict(blogs = blogs, user = user)
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
